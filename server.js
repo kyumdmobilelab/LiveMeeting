7365b1c4b6d3b7fa3357e5563ac2874af80c4e79
Your Name <you@example.com>
2017-10-03 13:15:19 +0800

    'fixed'

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -10,3 +11,25 @@ http.createServer(function (req, res) {
     }
     res.end();
 }).listen(process.env.PORT);
+*/
+
+
+// Load required modules
+var http   = require("http");     // https server core module
+var fs      = require("fs");        // file system core module
+var express = require("express");   // web framework external module
+var io      = require("socket.io"); // web socket external module
+var easyrtc = require(".");   // EasyRTC external module
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var httpApp = express();
+httpApp.use(express.static(__dirname + "/static/"));
+
+// Start Express https server on port 8443
+var webServer = http.createServer(httpApp).listen(process.env.PORT);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(httpApp, socketServer);
