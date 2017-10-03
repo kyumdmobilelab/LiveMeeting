64c82ee4565468be600b12bd8cf0a520ba57e1c1
Your Name <you@example.com>
2017-10-03 14:51:52 +0800

    'test'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Load required modules
-var https   = require("https");     // https server core module
+var http   = require("http");     // https server core module
 var fs      = require("fs");        // file system core module
 var express = require("express");   // web framework external module
 var io      = require("socket.io"); // web socket external module
@@ -10,7 +10,7 @@ var httpApp = express();
 httpApp.use(express.static(__dirname + "/static/"));
 
 // Start Express https server on port 8443
-var webServer = https.createServer({}, httpApp).listen(process.env.PORT);
+var webServer = http.createServer(httpApp).listen(process.env.PORT);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
