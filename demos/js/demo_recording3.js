08f66a07cf39b7d92e92c5732599b46c6339b3e4
Your Name <you@example.com>
2017-10-13 16:00:33 +0800

    'test'

---
+++ demos/js/demo_recording3.js
@@ -0,0 +1,415 @@
+var urlSearchParams = new URLSearchParams(location.search);
+
+if (urlSearchParams.get("user") === null || 
+    urlSearchParams.get("user") === "" ||
+    urlSearchParams.get("room") === null ||
+    urlSearchParams.get("room") === "") 
+{
+    if (/Mobi/.test(navigator.userAgent)) {
+        // mobile
+        window.location.href = "mobile.html";
+    } else {
+        // desktop
+        window.location.href = "index.html";
+    }
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(updateMyPosition);
+}
+
+function listButton_Click() {
+    if (document.getElementById("connectControls").style.display == 'none') {
+        document.getElementById("connectControls").style.display = 'block';
+    } else {
+        document.getElementById("connectControls").style.display = 'none';
+    }
+}
+
+function leaveRoomButton_Click() {
+    if (confirm("Leave this room?")) {
+        easyrtc.hangupAll();
+        let currentRoomName = urlSearchParams.get("room");
+        if (currentRoomName === null || currentRoomName === "")  {
+            currentRoomName = "default";
+        }
+        
+        easyrtc.leaveRoom(currentRoomName, 
+            function(roomName) {
+                console.log("No longer in room " + roomName);
+
+                if (urlSearchParams.get("os") == "android") {
+                    B4A.CallSub('leaveRoom', true);
+                    return;
+                }
+        
+                if (/Mobi/.test(navigator.userAgent)) {
+                    // mobile
+                    window.location.href = "mobile.html";
+                } else {
+                    // desktop
+                    window.location.href = "index.html";
+                }
+            },
+            function(errorCode, errorText, roomName) {
+                console.log("had problems leaving " + roomName);
+            });
+    }
+}
+
+var trackYourselfTimerId = null;
+var myCurrentLocation = null;
+var personMap = null;
+var markerArray = [];
+
+function initMap() {
+    let uluru = {lat: 25.0782782, lng: 121.537494};
+
+    personMap = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+
+    if ("geolocation" in navigator) {
+        //geolocation is available
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            //do_something(position.coords.latitude, position.coords.longitude);
+            personMap.setCenter(center)
+
+            let marker = new google.maps.Marker({
+                position: center,
+                title: "123456",
+                label: {text: "My Location", color: "#5151A2",  fontSize: "16px", fontWeight: "bold"},
+                icon: "images/marker-s.png",
+                map: personMap
+            });
+            markerArray.push(marker);
+        });
+    } else {
+        //geolocation IS NOT available
+    }
+
+}
+
+function updateMyPosition(position) {
+    console.log(position.coords.latitude + ", " + position.coords.longitude);
+    let pLabel = document.getElementById("MyPosition");
+    pLabel.innerText = "Current Location: " + position.coords.latitude + ", " + position.coords.longitude
+
+    myCurrentLocation = position;
+}
+
+function gotoMyLocation() {
+    let center = new google.maps.LatLng(myCurrentLocation.coords.latitude, myCurrentLocation.coords.longitude);
+    personMap.panTo(center);
+}
+
+function toggleTrackYourselfCheckbox(element) {
+    //console.log(element.checked)
+    if (element.checked == true) {
+        trackYourselfTimerId = window.setInterval(function() {
+            console.log("trackYourself Timer ...");
+            gotoMyLocation();
+        }, 3000);
+    } else {
+        if (trackYourselfTimerId != null) {
+            window.clearInterval(trackYourselfTimerId);
+        }
+    }
+}
+
+function toggleVideoDisableCheckbox(element) {
+    console.log(element.checked)
+    if (element.checked == true) {
+        easyrtc.enableVideo(false);
+    } else {
+        easyrtc.enableVideo(true);
+    }
+}
+
+function toggleAudioDisableCheckbox(element) {
+    console.log(element.checked)
+    if (element.checked == true) {
+        easyrtc.enableAudio(false);
+    } else {
+        easyrtc.enableAudio(true);
+    }
+}
+
+
+//-------------------------------------------------------------------//
+
+var selfEasyrtcid = "";
+var easyrtcidArray = null;
+var maxCALLERS = 3;
+
+function connect() {
+    // user name
+    if (urlSearchParams.get("user") != null) {
+        easyrtc.setUsername(urlSearchParams.get("user"));        
+    }
+
+    // room
+    if (urlSearchParams.get("room") != null) {
+        easyrtc.joinRoom(urlSearchParams.get("room"), null, null, null);
+    }
+
+    // hide recordButtons
+    if (urlSearchParams.get("hideRecord") == "y") {
+        document.getElementById("recordButtons").style.display = 'none';
+    } else {
+        document.getElementById("recordButtons").style.display = 'block';
+    }
+
+    // is mobile
+    if (urlSearchParams.get("isMobile") == "y") {
+        document.getElementById("recordButtons").style.display = 'none';
+        let trackYourselfCheckbox = document.getElementById("trackYourselfCheckbox");
+        trackYourselfCheckbox.checked = true;
+        toggleTrackYourselfCheckbox(trackYourselfCheckbox);
+    } else {
+        document.getElementById("recordButtons").style.display = 'block';
+        // document.getElementById("callerVideo").style.height = "320px";
+        // document.getElementById("callerVideo").style.width = "480px";
+        document.getElementById("trackYourself").style.display = 'none';
+    }
+
+    
+    if( !easyrtc.supportsRecording()) {
+       //window.alert("This browser does not support recording. Try chrome or firefox.");
+       document.getElementById("recordButtons").style.display = 'none';
+    } else {
+        if( easyrtc.isRecordingTypeSupported("h264")) document.getElementById("useH264").disabled = false;
+        if( easyrtc.isRecordingTypeSupported("vp9")) document.getElementById("useVP9").disabled = false;
+        if( easyrtc.isRecordingTypeSupported("vp8")) document.getElementById("useVP8").disabled = false;
+    }
+
+    
+    easyrtc.setPeerListener(addToConversation);
+    easyrtc.setVideoDims(640,480);
+    easyrtc.setRoomOccupantListener(convertListToButtons);
+    //easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+
+    easyrtc.easyApp("easyrtc.multiparty", "box0", ["box1", "box2", "box3"], loginSuccess);
+    easyrtc.setDisconnectListener( function() {
+        easyrtc.showError("LOST-CONNECTION", "Lost connection to signaling server");
+    });
+    easyrtc.setOnCall( function(easyrtcid, slot) {
+        console.log("getConnection count="  + easyrtc.getConnectionCount() );
+        // boxUsed[slot+1] = true;
+        // if(activeBox == 0 ) { // first connection
+        //     collapseToThumb();
+        //     document.getElementById('textEntryButton').style.display = 'block';
+        // }
+        document.getElementById(getIdOfBox(slot+1)).style.visibility = "visible";
+        // handleWindowResize();
+    });
+
+    easyrtc.setOnHangup(function(easyrtcid, slot) {
+        // boxUsed[slot+1] = false;
+        // if(activeBox > 0 && slot+1 == activeBox) {
+        //     collapseToThumb();
+        // }
+        setTimeout(function() {
+            document.getElementById(getIdOfBox(slot+1)).style.visibility = "hidden";
+
+            // if( easyrtc.getConnectionCount() == 0 ) { // no more connections
+            //     expandThumb(0);
+            //     document.getElementById('textEntryButton').style.display = 'none';
+            //     document.getElementById('textentryBox').style.display = 'none';
+            // }
+            // handleWindowResize();
+        },20);
+    });
+
+
+    // 每幾秒就更新一下地圖
+    window.setInterval(function() {
+        console.log("timer...");
+        if (personMap != null) {
+            // 刪除地圖裡的 marker
+            while(markerArray.length){
+                markerArray.pop().setMap(null);
+            }
+            //
+            //...
+        }
+    }, 8000);
+
+    //如果是 mobile 端，持續送出本地座標
+    if (urlSearchParams.get("isMobile") == "y") {
+        window.setInterval(function() {
+            console.log("mobile timer ...");
+            
+        }, 4000);
+    }
+}
+
+function getIdOfBox(boxNum) {
+    return "box" + boxNum;
+}
+
+
+function addToConversation(who, msgType, content) {
+    // Escape html special characters, then add linefeeds.
+    content = content.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    content = content.replace(/\n/g, '<br />');
+    let conversationObj = document.getElementById('conversation');
+    conversationObj.innerHTML += "<b>&nbsp;" + easyrtc.idToName(who) + ":</b>&nbsp;" + content + "<br />";
+    conversationObj.scrollTop = conversationObj.scrollHeight;
+}
+
+function sendMessageButton_Click(){
+    sendStuffP2P();
+}
+
+function sendStuffP2P() {
+    var text = document.getElementById('sendMessageText').value;
+    if(text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
+        return;
+    }
+
+    for(let easyrtcid in easyrtcidArray) {
+        easyrtc.sendPeerMessage(easyrtcid, "im",  text);
+    }
+    
+    addToConversation("Me", "message", text);
+    document.getElementById('sendMessageText').value = "";
+}
+
+
+function clearConnectList() {
+    var otherClientDiv = document.getElementById('otherClients');
+    while (otherClientDiv.hasChildNodes()) {
+        otherClientDiv.removeChild(otherClientDiv.lastChild);
+    }
+}
+
+
+function convertListToButtons (roomName, data, isPrimary) {
+    easyrtc.setRoomOccupantListener(null); // so we're only called once.
+
+    var list = [];
+    var connectCount = 0;
+    for(var easyrtcid in data ) {
+        list.push(easyrtcid);
+    }
+    //
+    // Connect in reverse order. Latter arriving people are more likely to have
+    // empty slots.
+    //
+    function establishConnection(position) {
+        function callSuccess() {
+            connectCount++;
+            if( connectCount < maxCALLERS && position > 0) {
+                establishConnection(position-1);
+            }
+        }
+        function callFailure(errorCode, errorText) {
+            easyrtc.showError(errorCode, errorText);
+            if( connectCount < maxCALLERS && position > 0) {
+                establishConnection(position-1);
+            }
+        }
+        easyrtc.call(list[position], callSuccess, callFailure);
+
+    }
+    if( list.length > 0) {
+        establishConnection(list.length-1);
+    }
+
+
+    /*
+    document.getElementById("roomname").innerHTML = "Room: " + roomName;
+    clearConnectList();
+    var otherClientDiv = document.getElementById('otherClients');
+    for(var easyrtcid in data) {
+        var button = document.createElement('button');
+        button.onclick = function(easyrtcid) {
+            return function() {
+                performCall(easyrtcid);
+            };
+        }(easyrtcid);
+
+        var label = document.createTextNode(easyrtc.idToName(easyrtcid));
+        button.appendChild(label);
+        otherClientDiv.appendChild(button);
+
+        let br = document.createElement("br");
+        otherClientDiv.appendChild(br);
+    }
+    */
+    easyrtcidArray = data;
+}
+
+
+function performCall(otherEasyrtcid) {
+    easyrtc.hangupAll();
+    var successCB = function() { };
+    var failureCB = function() {};
+    easyrtc.call(otherEasyrtcid, successCB, failureCB);
+}
+
+
+function loginSuccess(easyrtcid) {
+    selfEasyrtcid = easyrtcid;
+    if (easyrtc.username == null) {
+        document.getElementById("iam").innerHTML = "I am \"" + easyrtc.cleanId(easyrtcid) + "\".";
+    } else {
+        document.getElementById("iam").innerHTML = "I am \"" + easyrtc.username + "\".";
+    }
+    document.getElementById("startRecording").disabled = false;
+}
+
+
+function loginFailure(errorCode, message) {
+    easyrtc.showError(errorCode, message);
+}
+
+
+var selfRecorder = null;
+var callerRecorder = null;
+
+function startRecording() {
+    var selfLink = document.getElementById("selfDownloadLink");
+    selfLink.innerText = "";
+
+    selfRecorder = easyrtc.recordToFile( easyrtc.getLocalStream(), 
+               selfLink, "selfVideo");
+    if( selfRecorder ) {
+       document.getElementById("startRecording").disabled = true;
+       document.getElementById("stopRecording").disabled = false;
+    }
+    else {
+       window.alert("failed to start recorder for self");
+       return;
+    }
+
+    var callerLink = document.getElementById("callerDownloadLink");
+    callerLink.innerText = "";
+
+    if( easyrtc.getIthCaller(0)) {
+       callerRecorder = easyrtc.recordToFile(
+           easyrtc.getRemoteStream(easyrtc.getIthCaller(0), null), 
+             callerLink, "callerVideo");
+       if( !callerRecorder ) {
+          window.alert("failed to start recorder for caller");
+       }
+    }
+    else {
+       callerRecorder = null;
+    }
+}
+
+
+function endRecording() {
+    if( selfRecorder ) {
+       selfRecorder.stop();
+    }
+    if( callerRecorder ) {
+       callerRecorder.stop();
+    }
+    document.getElementById("startRecording").disabled = false;
+    document.getElementById("stopRecording").disabled = true;
+}
